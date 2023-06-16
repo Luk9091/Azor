22d18826c9068e4b7cd2ec49ce5bd6779b14212b
Łukasz Przystupa <lukasz9091s@gmail.com>
2023-06-16 10:29:38 +0200

    Fix dictionary concatenation

--- GUI/commands.py
+++ GUI/commands.py
@@ -1,25 +1,35 @@
 import sys
 from azor import Azor
+import serial.tools.list_ports as serialInfo
 # azor = Azor()
 
 class CLI:
     def __init__(self, azor: Azor) -> None:
-        self.COMMANDS = {
-            #  cmd      [[min, max] num of args, parameter args]
-            "forward":  {"minimumArgs": 0, "handler" : azor.forward},
-            "backward": {"minimumArgs": 0, "handler" : azor.backward},
-            "left":     {"minimumArgs": 0, "handler" : azor.turnLeft},
-            "right":    {"minimumArgs": 0, "handler" : azor.turnRight},
+        if azor.device.send != None:
+            COMMANDS = {
+                #  cmd      [[min, max] num of args, parameter args]
+                "forward":  {"minimumArgs": 0, "handler" : azor.forward},
+                "backward": {"minimumArgs": 0, "handler" : azor.backward},
+                "left":     {"minimumArgs": 0, "handler" : azor.turnLeft},
+                "right":    {"minimumArgs": 0, "handler" : azor.turnRight},
 
-            "head":     {"minimumArgs": 1, "handler" : self.head},
-            "acc":      {"minimumArgs": 1, "handler" : azor.Position.acceleration()},
-            "magnet":   {"minimumArgs": 1, "handler" : azor.Position.magneticField()},
-            "azimuth":  {"minimumArgs": 1, "handler" : azor.Position.azimuth},
+                "head":     {"minimumArgs": 1, "handler" : self.head},
+                "acc":      {"minimumArgs": 1, "handler" : azor.Position.acceleration()},
+                "magnet":   {"minimumArgs": 1, "handler" : azor.Position.magneticField()},
+                "azimuth":  {"minimumArgs": 1, "handler" : azor.Position.azimuth},
             
-            "distance": {"minimumArgs": 1, "handler" : self.distance},
-            "time":     {"minimumArgs": 0, "handler" : azor.getTime},
-            "velocity": {"minimumArgs": 0, "handler" : azor.getVelocity},
+                "distance": {"minimumArgs": 1, "handler" : self.distance},
+                "time":     {"minimumArgs": 0, "handler" : azor.getTime},
+                "velocity": {"minimumArgs": 0, "handler" : azor.getVelocity},
+            }
+        else:
+            COMMANDS = {}
+
+        terminalCmd = {
+            "connect"
         }
+
+        self.COMMANDS = terminalCmd | COMMANDS
 
 
 
@@ -29,13 +39,6 @@ class CLI:
         command = command.split(' ')
         cmd = command[0]
         args = command[1:]
-        if cmd == "exit":
-            # azor.disconnect()
-            sys.exit(0)
-
-        # for (index, arg) in enumerate(args):
-        #     if arg.isdigit():
-        #         args[index] = float(arg)
         
         if cmd in self.COMMANDS.keys():
             return self.COMMANDS[cmd]["handler"](*args)
@@ -65,6 +68,23 @@ class CLI:
             azor.device.send("ecr")
             return True
 
+    def connect(self, *args):
+        if len(args) == 0:
+            azor.device.showDevices()
+        else:
+            ports = serialInfo.comports()
+            for port in ports:
+                if port.device == args[0]:
+                    break
+            else:
+                return "Invalid port name"
+            
+            azor.device.connect(args[0])
+
+    def exit(self):
+        sys.exit()
+
+    
 
 
 if __name__ == "__main__":
